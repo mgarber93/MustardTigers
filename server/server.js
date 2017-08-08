0fc9a046d32a01bab22ecf402cef499ce146e5df
Matt <mgarber93@gmail.com>
2017-08-08 16:33:20 -0700

    Rebase

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cookieParser = require('cookie-parser'); 
 const bodyParser = require('body-parser'); 
 const User = require('../database/models/user');
 const session = require('express-session');
 const Store = require('connect-session-sequelize')(session.Store);
-const {db} = require('../database/connection');
+const { db } = require('../database/connection');
 const app = express();
 
 /**
@@ -15,6 +16,10 @@ const store = new Store({
 
 store.sync();
 
+
+const usersRouter = require('./users');
+
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -35,111 +40,8 @@ app.use(session({
  */
 app.use(express.static(__dirname + '/../client/build'));
 
-/**
- * A get request to the users endpoint returns all users as an array of json
- * objects. A post to users creates a new user and returns the new user's id as 
- * the id property of the reponse.
- * 
- * @param  {function} (req, res, next) - Request handler 
- */
-app.route('/users')
-  .get((req, res, next) => {
-    User.findAll() 
-      .then(doc => {
-        res.status(200);
-        res.json({results: doc});
-        res.end();
-      })
-      .catch(except => {
-        res.status(400);
-        res.end(except.message || 'Unable to fetch users!');
-      })
-      .error(error => {
-        res.status(500);
-        res.end(error.message || 'Internal error');
-      });
-  })
-  .post((req, res, next) => {
-    User.create({
-      username: req.body.username, 
-      password: req.body.password
-    })
-      .then(doc => {
-        req.session.userId = doc.id;
-        res.status(200);
-        res.json({id: doc.id});
-        res.end();
-      })
-      .catch(except => {
-        res.status(400);
-        res.end(except.message || 'Unable to create user!');
-      })
-      .error(error => {
-        res.status(500);
-        res.end(error.message || 'Internal error');
-      });
-  });
+app.use('/users', usersRouter);
 
-
-/**
- * Read, update, or delete a specific user by sending a get, post, or delete
- * verb to the user's endpoint. On success, post and delete will return 
- * the changed user's id.
- * 
- * @param  {function} (req, res, next) - Request handler 
- */
-app.route('/users/:user')
-  .get((req, res, next) => {
-    User.read({id: req.params.user}) 
-      .then(doc => {
-        res.status(200);
-        res.json({results: doc});
-        res.end();
-      })
-      .catch(except => {
-        res.status(400);
-        res.end(except.message || 'Unable to fetch users!');
-      })
-      .error(error => {
-        res.status(500);
-        res.end(error.message || 'Internal error');
-      });
-  })
-  .post((req, res, next) => {
-    User.update({id: req.params.id}, {
-      username: req.body.username,
-      password: req.body.password,
-    })
-      .then(doc => {
-        res.status(200);
-        res.json({id: doc.id});
-        res.end();
-      })
-      .catch(except => {
-        res.status(except.status || 400);
-        res.end(except.message || 'Unable to update user!');
-      })
-      .error(error => {
-        res.status(500);
-        res.end(error.message || 'Internal error');
-      });
-  })
-  .delete((req, res, next) => {
-    User.delete({id: req.params.user})
-      .then(doc => {
-        res.status(200);
-        res.json({id: req.params.user});
-        res.end();
-      })
-      .catch(except => {
-        res.status(except.status || 400);
-        res.end(except.message || 'Unable to delete users!');
-      })
-      .error(error => {
-        res.status(500);
-        res.end(error.message || 'Internal error');
-      });
-  });
 
 
 module.exports.app = app;
