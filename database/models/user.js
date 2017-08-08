9e565e4d5afdc6592faf6a455e760aa715459e3c
Ian Mobley <imobley@mobsworkshop.com>
2017-08-07 22:10:27 -0700

    Add some basic tests and trying to set up travis
    This is using the basic schema. I just want to make sure that environment
    variables are being set correctly.

--- database/models/user.js
+++ database/models/user.js
@@ -1,7 +1,13 @@
 const crypto = require('crypto');
 const {Sequelize, db} = require('../connection');
 
-const User = db.define('users', {
+const hashData = function(data, salt = '') {
+  var shasum = crypto.createHash('sha256');
+  shasum.update(data + salt);
+  return shasum.digest('hex');
+};
+
+const UserModel = db.define('users', {
   username: {
     type: Sequelize.STRING(40),
     allowNull: false,
@@ -13,10 +19,19 @@ const User = db.define('users', {
   },
   salt: {
     type: Sequelize.STRING(64),
-    allowNull: false
+  }
+}, {
+  hooks: {
+    beforeCreate: function(user) {
+      user.salt = crypto.randomBytes(32).toString('hex');
+
+      user.password = hashData(user.password, user.salt);
+    }
   }
 });
 
-User.sync();
+UserModel.beforeCreate
 
-module.exports = User;
+UserModel.sync();
+
+module.exports = UserModel;
--- tests/user.js
+++ tests/user.js
@@ -1,10 +1,23 @@
-const database = require('../database');
+const {User} = require('../database');
 const expect = require('chai').expect;
 
-xdescribe('User schema', function() {
-  it('inserts new users', function() {
+describe('User schema', function() {
+  beforeEach(function(done) {
+    User.truncate()
+      .then(done);
   });
 
-  it('does not allow duplicate users', function() {
+  it('inserts new users', function(done) {
+    var user = {username: 'John Doe', password: 'John Doe'};
+
+    User.create(user)
+      .then(function(newUser) {
+        expect(newUser.username).to.exist;
+
+        done();
+      });
+  });
+
+  xit('does not allow duplicate users', function(done) {
   });
 });
