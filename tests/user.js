a9733f331833f61bed5aac3d32369473d3f4e1b8
Ian Mobley <imobley@mobsworkshop.com>
2017-08-07 20:17:06 -0700

    Remove bs test, stub out some real tests

---
+++ tests/user.js
@@ -0,0 +1,10 @@
+const database = require('../database');
+const expect = require('chai').expect;
+
+xdescribe('User schema', function() {
+  it('inserts new users', function() {
+  });
+
+  it('does not allow duplicate users', function() {
+  });
+});
