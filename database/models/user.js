eb7718dec90aa8103b3241b59c965f24863017db
Ian Mobley <imobley@mobsworkshop.com>
2017-08-07 20:05:31 -0700

    Flesh out user schema

--- database/models/user.js
+++ database/models/user.js
@@ -1,11 +1,19 @@
+const crypto = require('crypto');
 const {Sequelize, db} = require('../connection');
 
 const User = db.define('users', {
   username: {
+    type: Sequelize.STRING(40),
+    allowNull: false,
+    unique: true
   },
   password: {
+    type: Sequelize.STRING(64),
+    allowNull: false
   },
   salt: {
+    type: Sequelize.STRING(64),
+    allowNull: false
   }
 });
 
