296efca1760f0c074507d20ee03a8ee38458dad5
Matt <mgarber93@gmail.com>
2017-08-09 14:55:32 -0700

    Fix Merge

--- client/src/components/auth/LoginForm.jsx
+++ client/src/components/auth/LoginForm.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-<<<<<<< HEAD
 import {Link} from 'react-router-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Button, ControlLabel, Form, FormGroup, FormControl } from 'react-bootstrap';
-=======
-import { Button, Glyphicon, Jumbotron } from 'react-bootstrap';
-import FormControl from 'react-bootstrap/lib/FormControl';
-import { FieldGroup, FormGroup, ControlLabel } from 'react-bootstrap';
-import TextFieldGroup from '../common/TextFieldGroup.jsx';
->>>>>>> Correct style mistakes
 /**
  * Class representing the React Login Component.
  * @extends Login
@@ -34,41 +27,16 @@ class LoginForm extends React.Component {
 
   render () {
     return (
-<<<<<<< HEAD
       <Grid>
         <Row>
         Username 
-        <input name="username" placeholder="Bubbles" onChange={this.handleChange.bind(this)}/>
+          <input name="username" placeholder="Bubbles" onChange={this.handleChange.bind(this)}/>
         Password:
-        <input name="password" type="password" placeholder="" onChange={this.handleChange.bind(this)}/>
-        <button onClick={this.handleSubmit.bind(this)}>Submit</button>
+          <input name="password" type="password" placeholder="" onChange={this.handleChange.bind(this)}/>
+          <button onClick={this.handleSubmit.bind(this)}>Submit</button>
         </Row>
       </Grid>
-    )
-=======
-      <form onSubmit={this.onSubmit}>
-        <h1>Login</h1>
-        <TextFieldGroup
-          field="identifier"
-          label="Username / Email"
-          value={identifier}
-          error={errors.identifier}
-          onChange={this.onChange}
-        />
-
-        <TextFieldGroup
-          field="password"
-          label="Password"
-          value={password}
-          error={errors.password}
-          onChange={this.onChange}
-          type="password"
-        />
-
-        <div className="form-group"><button className="btn btn-primary btn-lg" disabled={isLoading}>Login</button></div>
-      </form>
     );
->>>>>>> Correct style mistakes
   }
 }
 export default LoginForm;
