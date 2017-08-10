ca69cea2df81b353cac254d911581a684e05e8a3
Ian Mobley <imobley@mobsworkshop.com>
2017-08-10 15:05:15 -0700

    Implement root route get/post

--- server/clans.js
+++ server/clans.js
@@ -1,15 +1,33 @@
 const router = require('express').Router();
+const {Clan} = require('../database');
 
 router.get('/', (req, res) => {
+  return Clan.findAll(req.query)
+    .then(clans => {
+      res.json(clans);
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
+    });
 });
 
 router.post('/', (req, res) => {
+  return Clan.create(req.body)
+    .then(newClan => {
+      res.status(201).json(newClan);
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
+    });
 });
 
 router.get('/:clan', (req, res) => {
 });
 
 router.post('/:clan', (req, res) => {
+});
+
+router.delete('/:clan', (req, res) => {
 });
 
 module.exports = router;
