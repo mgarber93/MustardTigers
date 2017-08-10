f2604d3e1f5b5b031c3720c67d6b3b45717388c9
Matt <mgarber93@gmail.com>
2017-08-10 14:18:25 -0700

    Refactor sanitize code for readability

--- database/models/user.js
+++ database/models/user.js
@@ -39,36 +39,30 @@ UserModel.sync();
 var User = {model: UserModel};
 
 /**
- * User _sanitize is a private helper function that sanitizes the output
- * of the model's CRUD functions. For user table a whitelist strategy is
+ * Sanitize is a private helper function that sanitizes the output
+ * of the model's CRUD functions. For user's table a whitelist strategy is
  * used to filter out password<hash> and salt<hash> data.
  * 
  * @param <object> - A rowData object
  */
-User._sanitize = function({id, username, createdAt, updatedAt}) {
+const sanitize = function({id, username, createdAt, updatedAt}) {
   return {id, username, createdAt, updatedAt};
 };
 
 User.create = function({username, password}) {
   return UserModel.find({where: {username}})
     .then(function(user) {
-      if (user) {
-        throw new Error('User already exists');
-      }
-
-      return UserModel.create({username, password})
-        .then(newUser => {
-          newUser = User._sanitize(newUser);
-          return Promise.resolve(newUser);
-        });
-    });
+      if (user) { throw new Error('User already exists'); }
+      return UserModel.create({username, password});
+    })
+    .then(sanitize);
 };
 
 User.validate = function({username, password}) {
   return UserModel.findOne({where: {username}})
     .then(function(user) {
       if (user && user.password === hashData(password, user.salt)) {
-        return User._sanitize(user);
+        return sanitize(user);
       } else {
         return null;
       }
@@ -76,26 +70,25 @@ User.validate = function({username, password}) {
 };
 
 User.findAll = function() {
-  return UserModel.findAll().map(User._sanitize);
+  return UserModel.findAll().map(sanitize);
 };
 
 User.read = function({id, username}) {
   return UserModel.find({where: {id}})
-    .then(newUser => Promise.resolve(User._sanitize(newUser)));
+    .then(sanitize);
 };
 
 /**
- * No test coverage!
- * @todo testing
+ * @todo test coverage for update!
  */
 User.update = function(query, values) {
   return UserModel.update({values: values}, {where: query})
-    .then(doc => Promise.resolve(doc.map(User._sanitize)));
+    .then(doc.map(sanitize));
 };
 
 User.delete = function(query) {
   return UserModel.destroy({where: query})
-    .then(doc => Promise.resolve(User._sanitize(doc)));
+    .then(sanitize);
 };
 
 module.exports = User;
