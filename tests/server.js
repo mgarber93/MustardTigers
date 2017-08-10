a3b4573200b5eeb98bd77073c0c9555cc3b9fe17
Matt <mgarber93@gmail.com>
2017-08-10 12:11:48 -0700

    Implement data sanitation with test coverage
    Using a helper function was the fastest path to achieve this functionality. I will look into using hooks for the other models. Small changes were needed to middleware test coverage.

--- tests/server.js
+++ tests/server.js
@@ -1,12 +1,25 @@
 const supertest = require('supertest');
 const { app } = require('../server/server');
 const request = supertest.agent(app);
+const { User } = require('../database');
+const {Sequelize, db} = require('../database/connection');
 
 /**
  * @todo Double check with the database that changes went through.
  */
 describe('', function() {
   describe('Express Middleware', function() {
+    
+    var user = {username: 'fred_zirdung', password: 'fred_zirdung'};
+
+    beforeEach(function(done) {
+      db.clearDb()
+        .then(() => { done(); })
+        .catch(err => {
+          console.error(err);
+          done(err);
+        });
+    });
 
     it('should have index.html', function(done) {
       request.get('/')
@@ -20,9 +33,12 @@ describe('', function() {
     });
 
     it('should return users from get to /users/:user', function(done) {
-      request.get('/users/1')
-        .expect(200)
-        .expect('Content-Type', /json/, done);
+      User.create(user)
+        .then(newUser => {
+          request.get(`/users/${newUser.id}`)
+            .expect(200)
+            .expect('Content-Type', /json/, done);
+        })
     });
 
     it('should create a new user with post to /users ', function(done) {
@@ -33,8 +49,11 @@ describe('', function() {
     });
 
     it('should delete a new user with delete to /users ', function(done) {
-      request.delete('/users/0')
-        .expect(200, done);
+      User.create(user)
+        .then(newUser => {
+          request.delete(`/users/${newUser.id}`)
+            .expect(200);
+        });
     });
 
   });
--- tests/user.js
+++ tests/user.js
@@ -3,6 +3,7 @@ const {expect} = require('chai');
 const {Sequelize, db} = require('../database/connection');
 
 var user = {username: 'fred_zirdung', password: 'fred_zirdung'};
+var user2 = {username: 'test_user_please_ignore', password: 'test_user_please_ignore'};
 
 describe('User Schema', function() {
   beforeEach(function(done) {
@@ -15,12 +16,12 @@ describe('User Schema', function() {
   });
 
   it('inserts new users', function(done) {
-    User.create(user)
+    User.model.create(user)
       .then(function(newUser) {
+        expect(newUser.id).to.exist;
         expect(newUser.username).to.exist;
         expect(newUser.salt).to.exist;
         expect(newUser.password).to.not.equal(user.password);
-
         done();
       }).catch(done);
   });
@@ -46,5 +47,50 @@ describe('User Schema', function() {
         done();
       })
       .catch(done);
+  });
+
+
+  it ('sanitizes user data on create', function(done) {
+    User.create(user)
+      .then(function(newUser) {
+        expect(newUser.id).to.exist;
+        expect(newUser.username).to.exist;
+        expect(newUser.salt).to.not.exist;
+        expect(newUser.password).to.not.exist;
+        done();
+      }).catch(done);
+  });
+
+  it ('sanitizes user data on read', function(done) {
+    User.create(user)
+      .then(({id}) => {
+        return User.read({id});
+      })
+      .then(function(newUser) {
+        expect(newUser.id).to.exist;
+        expect(newUser.username).to.exist;
+        expect(newUser.salt).to.not.exist;
+        expect(newUser.password).to.not.exist;
+        done();
+      }).catch(done);
+  });
+
+  it ('sanitizes user data on findAll', function(done) {
+    User.create(user)
+      .then(() => {
+        return User.create(user2);
+      })
+      .then(() => {
+        return User.findAll();
+      })
+      .then(users => {
+        users.forEach(user => {
+          expect(user.id).to.exist;
+          expect(user.username).to.exist;
+          expect(user.salt).to.not.exist;
+          expect(user.password).to.not.exist;
+        });
+        done();
+      }).catch(done);
   });
 });
