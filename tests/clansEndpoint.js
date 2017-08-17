0c94e36173e733aa0ae26cb26315e80930b32428
Ian Mobley <imobley@mobsworkshop.com>
2017-08-17 13:06:43 -0700

    Refactor tests to be promise based

--- tests/clansEndpoint.js
+++ tests/clansEndpoint.js
@@ -3,87 +3,80 @@ const {app} = require('../server/server');
 const request = require('supertest').agent(app);
 const {db} = require('../database/connection');
 
-// @todo this should probably go in a helper library
-var clearDb = function(done) {
-  db.clearDb().then(() => done()).catch(done);
-};
-
 var testClan = {name: 'test_clan_please_ignore'};
 
 describe('Clans API Endpoint', function() {
-  beforeEach(clearDb);
+  beforeEach(function() {
+    return db.sync({force: true});
+  });
 
-  it('should retrieve an array', function(done) {
-    request.get('/api/clans')
-      .expect(res => {
+  it('should retrieve an array', function() {
+    return request.get('/api/clans')
+      .expect(200)
+      .then(res => {
         expect(res.body.results).to.exist;
         expect(res.body.results).to.be.an('array');
-      })
-      .expect(200, done);
+      });
   });
 
-  it('should insert new clans', function(done) {
-    request.post('/api/clans')
+  it('should insert new clans', function() {
+    return request.post('/api/clans')
       .send(testClan)
-      .expect(res => {
+      .expect(201)
+      .then(res => {
         expect(res.body.name).to.equal(testClan.name);
-      })
-      .expect(201, done);
+      });
   });
 
-  it('should retrieve existing clans', function(done) {
+  it('should retrieve existing clans', function() {
     var newClan;
 
-    request.post('/api/clans')
+    return request.post('/api/clans')
       .send(testClan)
       .expect(201)
       .then(res => {
         newClan = res.body;
 
         return request.get('/api/clans')
-          .expect(res => {
-            expect(res.body.results.length).to.equal(1);
-            expect(res.body.results[0].id).to.equal(newClan.id);
-            expect(res.body.results[0].name).to.equal(newClan.name);
-          })
           .expect(200);
       })
-      .then(() => {
-        request.get(`/api/clans/${newClan.id}`)
-          .expect(res => {
-            expect(res.body.results).to.exist;
-            expect(res.body.results.id).to.equal(newClan.id);
-            expect(res.body.results.name).to.equal(newClan.name);
-          })
-          .expect(200, done);
+      .then(res => {
+        expect(res.body.results.length).to.equal(1);
+        expect(res.body.results[0].id).to.equal(newClan.id);
+        expect(res.body.results[0].name).to.equal(newClan.name);
       })
-      .catch(done);
+      .then(() => {
+        return request.get(`/api/clans/${newClan.id}`)
+          .expect(200);
+      })
+      .then(res => {
+        expect(res.body.results).to.exist;
+        expect(res.body.results.id).to.equal(newClan.id);
+        expect(res.body.results.name).to.equal(newClan.name);
+      });
   });
 
-  it('should retrieve existing clans with a query', function(done) {
-    request.post('/api/clans')
+  it('should retrieve existing clans with a query', function() {
+    return request.post('/api/clans')
       .send(testClan)
       .expect(201)
       .then(res => {
         newClan = res.body;
 
         return request.get('/api/clans')
-          .query({name: testClan.name})
-          .expect(res => {
-            expect(res.body.results.length).to.equal(1);
-            expect(res.body.results[0].id).to.equal(newClan.id);
-            expect(res.body.results[0].name).to.equal(newClan.name);
-          })
-          .expect(200);
+          .query({name: testClan.name});
+      })
+      .then(res => {
+        expect(res.body.results.length).to.equal(1);
+        expect(res.body.results[0].id).to.equal(newClan.id);
+        expect(res.body.results[0].name).to.equal(newClan.name);
       })
       .then(() => {
-        request.get('/api/clans')
-          .query({name: 'Barbara Streisand'})
-          .expect(res => {
-            expect(res.body.results.length).to.equal(0);
-          })
-          .expect(200, done);
+        return request.get('/api/clans')
+          .query({name: 'Barbara Streisand'});
       })
-      .catch(done);
+      .then(res => {
+        expect(res.body.results.length).to.equal(0);
+      });
   });
 });
