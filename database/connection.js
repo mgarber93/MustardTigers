1d6ac27ae3f5f6bbb00e5a3d9987f904e27dccba
Matt <mgarber93@gmail.com>
2017-08-16 19:14:27 -0700

    Enable logging

--- database/connection.js
+++ database/connection.js
@@ -3,23 +3,27 @@ const Sequelize = require('sequelize');
 const database = process.env.DATABASE_URL;
 
 const db = new Sequelize(database, {
-  logging: false,
+  // logging: false,
   force: true
 });
 
 db.clearDb = () => {
   return db.transaction((t) => {
     const options = { raw: true, transaction: t };
-    return Promise.resolve(db)
-      .then(function() {
-        return Promise.all([
-          db.query('delete from users', null, options),
-          db.query('delete from forums', null, options),
-          db.query('delete from posts', null, options),
-          db.query('delete from members', null, options),
-          db.query('delete from clans', null, options),
-          db.query('delete from postVotes', null, options),
-        ]);
+
+    return db.sync()
+      .then(() => {
+        return Promise.resolve(db)
+          .then(function() {
+            return Promise.all([
+              db.query('delete from users', null, options),
+              db.query('delete from forums', null, options),
+              db.query('delete from posts', null, options),
+              db.query('delete from members', null, options),
+              db.query('delete from clans', null, options),
+              db.query('delete from postVotes', null, options),
+            ]);
+          });
       });
   });
 };
