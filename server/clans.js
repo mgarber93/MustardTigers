50d3bc23ab97abc541661e322dc11432bacaee49
Ian Mobley <imobley@mobsworkshop.com>
2017-08-17 13:44:16 -0700

    Add members endpoint, move around gated data

--- server/clans.js
+++ server/clans.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {Clan} = require('../database');
+const {Clan, User} = require('../database');
 
 router.get('/', (req, res) => {
   return Clan.findAll(req.query)
@@ -11,20 +11,37 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
-  return Clan.create(req.body)
-    .then(newClan => {
-      res.status(201).json(newClan);
+router.get('/:clan', (req, res) => {
+  return Clan.read({id: req.params.clan})
+    .then(clan => {
+      res.json({results: clan.toJSON()});
     })
     .catch(err => {
       res.status(500).send(err.message);
     });
 });
 
-router.get('/:clan', (req, res) => {
-  return Clan.read({id: req.params.clan})
-    .then(clan => {
-      res.json({results: clan});
+router.get('/:clan/members', (req, res) => {
+  Clan.model.findOne({
+    include: [{
+      model: User.model
+    }],
+    where: {id: req.params.clan}
+  })
+    .then(({users}) => {
+      res.json({results: users.map(member => member.toJSON())});
+    })
+    .catch(err => {
+      res.status(500).send(err.message);
+    });
+});
+
+// Validation middleware goes here
+
+router.post('/', (req, res) => {
+  return Clan.create(req.body)
+    .then(newClan => {
+      res.status(201).json(newClan);
     })
     .catch(err => {
       res.status(500).send(err.message);
