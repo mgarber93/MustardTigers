1d753151838efef5111feeb53b0919e61ec8b97c
Matt <mgarber93@gmail.com>
2017-08-20 12:13:30 -0700

    Implement limit to clans a user can create

--- database/models/clan.js
+++ database/models/clan.js
@@ -27,6 +27,8 @@ Clan.findAll = function(query = {}) {
   return ClanModel.findAll({where: query});
 };
 
+const MAX_CLANS_PER_USER = 5;
+
 /**
  * Clan crud methods.
  */
@@ -35,6 +37,12 @@ Clan.create = function({name, creatorId, tag, avatar, description}) {
     .then(function(clan) {
       if (clan) {
         throw new Error('Clan already exists');
+      }
+      return ClanModel.findAll({where: {creatorId}});
+    })
+    .then(function(clans) {
+      if (clans.length + 1 > MAX_CLANS_PER_USER) {
+        throw new Error('User has created too many clans!'); 
       }
       return ClanModel.create({name, creatorId, tag, avatar, description});
     });
